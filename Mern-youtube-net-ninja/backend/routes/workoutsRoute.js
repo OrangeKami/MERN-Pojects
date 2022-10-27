b4da3bd60b4f07bc3b59bd8da564af3e5badc51a
Chen <97513965+OrangeKami@users.noreply.github.com>
2022-10-28 08:28:40 +1100

    routes update
    Routes

--- Mern-youtube-net-ninja/backend/routes/workoutsRoute.js
+++ Mern-youtube-net-ninja/backend/routes/workoutsRoute.js
@@ -6,8 +6,25 @@ router.get('/', (req, res) => {
     res.json({ message:"Workouts"})
 })
 
+// * get a single workeout
 router.get("/:id", (req, res) => {
-  res.json({ message: "Workouts by id" });
+  res.json({ message: "Workout by id" });
 });
+
+//  * post a new workeout
+router.post("/", (req, res) => {
+    res.json({ message: "Post new workeout" });
+});
+
+// * delete  a workeout
+router.delete("/:id", (req, res) => {
+  res.json({ message: "Delete workeout" });
+});
+
+// *update a workeout
+router.put("/:id", (req, res) => {
+  res.json({ message: "Update workeout" });
+});
+
 
 export default router;
